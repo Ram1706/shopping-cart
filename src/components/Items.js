4d07631074fefdf2c7d322f9d1233c1e09b9ffca
Ramkumar <ramsksra@gmail.com>
2019-11-11 09:25:41 +0530

    changes

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,48 +1,52 @@
- const Items=[
-	{
-        product:"Cell",
-        price:400,
-        ogPrice:400,
-	color: "red",
-        value: "#f00",
-        size:"s",
-        qty:1,
-        availableSize:["S","M","L","XL","XXL"],
-        url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHHpn4OagOgf9jfSeuRAF0jkbwtwRXks3tWkDIxVbx57VjY6E17Q&s"
-	},
-	{
-        product:"dress",
-        price:800,
-        ogPrice:800,
-	color: "green",
-        value: "#0f0",
-        size:"M",
-        qty:1,
-        availableSize:["S","M","L","XL","XXL"],
-        url:"https://store.sabaton.net/wp-content/uploads/2019/03/bismarck-tshirt-front-sabaton-T19010.png"
-	},
-	{
-        product:"shoe",
-        color: "blue",
-        price:500,
-        ogPrice:500,
-        value: "#00f",
-        size:"L",
-        qty:1,
-        availableSize:["S","M","L","XL","XXL"],
-        url:"https://images-na.ssl-images-amazon.com/images/I/618wh3ieU-L._UX679_.jpg"
-	},
-	{
-        product:"pen",
-        color: "cyan",
-        price:10,
-        ogPrice:10,
-        value: "#0ff",
-        size:"XL",
-        qty:1,
-        availableSize:["S","M","L","XL","XXL"],
-        url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHHpn4OagOgf9jfSeuRAF0jkbwtwRXks3tWkDIxVbx57VjY6E17Q&s"
-	}
+const Items = [
+  {
+    product: "Black-T-Shirt",
+    price: 400,
+    ogPrice: 400,
+    color: "black",
+    value: "#f00",
+    size: "s",
+    qty: 1,
+    availableSize: ["S", "M", "L", "XL", "XXL"],
+    url:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHHpn4OagOgf9jfSeuRAF0jkbwtwRXks3tWkDIxVbx57VjY6E17Q&s"
+  },
+  {
+    product: "LP-Black-T-shirt",
+    price: 800,
+    ogPrice: 800,
+    color: "black",
+    value: "#0f0",
+    size: "M",
+    qty: 1,
+    availableSize: ["S", "M", "L", "XL", "XXL"],
+    url:
+      "https://store.sabaton.net/wp-content/uploads/2019/03/bismarck-tshirt-front-sabaton-T19010.png"
+  },
+  {
+    product: "Red T-Shirt",
+    color: "Red",
+    price: 500,
+    ogPrice: 500,
+    value: "#00f",
+    size: "L",
+    qty: 1,
+    availableSize: ["S", "M", "L", "XL", "XXL"],
+    url:
+      "https://images-na.ssl-images-amazon.com/images/I/618wh3ieU-L._UX679_.jpg"
+  },
+  {
+    product: "Navy Black",
+    color: "black",
+    price: 10,
+    ogPrice: 10,
+    value: "#0ff",
+    size: "M",
+    qty: 1,
+    availableSize: ["S", "M", "L", "XL", "XXL"],
+    url:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHHpn4OagOgf9jfSeuRAF0jkbwtwRXks3tWkDIxVbx57VjY6E17Q&s"
+  }
 ];
 
 export default Items;
